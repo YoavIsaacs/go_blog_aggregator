f105e0f6e5a04bc3286a28705aba119ee5aa02ca
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-03-21 23:25:51 +0200

    Added config handling functions

--- internal/config/config_handler.go
+++ internal/config/config_handler.go
@@ -1,3 +1,34 @@
 package config
 
-type Config struct{}
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Config struct {
+	DatabaseURL string `json:"db_url"`
+}
+
+func Read() Config {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("Error getting home directory...")
+	}
+
+	byteValue, err := os.ReadFile(homeDir + ".gatorconfig")
+	if err != nil {
+		log.Fatal("Error reading config file...")
+	}
+
+	var config Config
+
+	err = json.Unmarshal(byteValue, &config)
+	if err != nil {
+		log.Fatal("Error unmarshlling config file...")
+	}
+
+	fmt.Println(config.DatabaseURL)
+	return config
+}
