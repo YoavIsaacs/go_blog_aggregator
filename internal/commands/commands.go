97cf25244841a2e6e37b673d970428aa3734a334
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-03-22 22:06:43 +0200

    added login command

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/YoavIsaacs/go_blog_aggregator/internal/config"
 )
 
@@ -11,4 +13,18 @@ type state struct {
 type command struct {
 	commandName string
 	args        []string
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.args) == 0 {
+		return fmt.Errorf("expected 1 arguments, the username\ncorrect usage: login [USERNAME]")
+	}
+
+	err := s.config.SetUser(cmd.args[0])
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("The username has beed set successfully. Username is now %v", cmd.args[0])
+	return nil
 }
