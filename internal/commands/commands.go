c12e6b75afd18ff6bfde8aacff51c62106ea44e1
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-03-22 22:28:27 +0200

    added commands + related functions

--- internal/commands/commands.go
+++ internal/commands/commands.go
@@ -15,6 +15,10 @@ type command struct {
 	args        []string
 }
 
+type commands struct {
+	allCommands map[string]func(*state, command) error
+}
+
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return fmt.Errorf("expected 1 argument, the username\ncorrect usage: login [USERNAME]")
@@ -26,5 +30,17 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	fmt.Printf("The username has beed set successfully. Username is now %v", cmd.args[0])
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.allCommands[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.allCommands[cmd.commandName](s, cmd)
+	if err != nil {
+		return err
+	}
 	return nil
 }
