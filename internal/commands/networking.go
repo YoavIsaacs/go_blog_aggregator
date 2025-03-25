dfc1d35696d249e06795b277684491acf3992056
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-03-25 22:01:33 +0200

    added get RSS feed function

--- internal/commands/networking.go
+++ internal/commands/networking.go
@@ -1,5 +1,14 @@
 package commands
 
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
 type RSSFeed struct {
 	Title       string    `xml:"title"`
 	Link        string    `xml:"link"`
@@ -12,4 +21,41 @@ type RSSItem struct {
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
 	PubDate     string `xml:"pubDate"`
+}
+
+func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error: failed to create request: %w", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error: failed to fetch the feed: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error: failed to read the response body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: unexpected status code: %d", resp.StatusCode)
+	}
+
+	var rssFeed RSSFeed
+	err = xml.Unmarshal(body, &rssFeed)
+	if err != nil {
+		return nil, fmt.Errorf("error: failed to unmarshal RSS feed: %w", err)
+	}
+
+	return &rssFeed, nil
 }
