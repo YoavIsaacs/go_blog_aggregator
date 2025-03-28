9f1c0a10e04cd64d65371314c92e322266f040d7
Yoav Isaacs <yoav.isaacs@gmail.com>
2025-03-28 21:42:41 +0300

    fixed missing tadatype

--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -4,7 +4,7 @@ CREATE TABLE feeds (
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  name NOT NULL,
+  name TEXT NOT NULL,
   url TEXT UNIQUE NOT NULL,
   user_id UUID NOT NULL,
   FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
